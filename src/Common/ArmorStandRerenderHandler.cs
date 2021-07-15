a19ec6231123b61e52c3c02d265e7323fe8245c9
JapanHasRice <JapanHasRice@gmail.com>
2021-07-15 00:38:35 -0400

    Updates the rerender logic to be version-dependent.
    As of 1.15.0-rc.4, the armor stand render bug was fixed, which also fixed the need to manually trigger a rerender for this mod.

--- src/Common/ArmorStandRerenderHandler.cs
+++ src/Common/ArmorStandRerenderHandler.cs
@@ -1,17 +1,24 @@
 using DoffAndDonAgain.Common.Network;
 using DoffAndDonAgain.Utility;
+using ProperVersion;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.GameContent;
 
 namespace DoffAndDonAgain.Common {
   public class ArmorStandRerenderHandler {
     private DoffAndDonSystem System { get; }
+    private bool shouldSkipRerender;
+
+    private static readonly SemVer armorStandRenderFixVersion = SemVer.Parse("1.15.0-rc.4");
 
     public ArmorStandRerenderHandler(DoffAndDonSystem system) {
       System = system;
       if (System.Side == EnumAppSide.Client) {
         System.ClientChannel.SetMessageHandler<ArmorStandInventoryUpdatedPacket>(OnArmorStandInventoryUpdatedPacket);
       }
+      var gameVersion = SemVer.Parse(GameVersion.OverallVersion);
+      shouldSkipRerender = gameVersion >= armorStandRenderFixVersion;
     }
 
     private void OnArmorStandInventoryUpdatedPacket(ArmorStandInventoryUpdatedPacket packet) {
@@ -21,7 +28,7 @@ namespace DoffAndDonAgain.Common {
     }
 
     public void UpdateRender(EntityArmorStand armorStand) {
-      if (armorStand == null) { return; }
+      if (shouldSkipRerender || armorStand == null) { return; }
       if (armorStand.World.Side == EnumAppSide.Server) {
         armorStand.WatchedAttributes.MarkAllDirty();
         Action<float> broadcastCallback = (float timePassed) => { System.ServerChannel.BroadcastPacket(new ArmorStandInventoryUpdatedPacket(armorStand.EntityId)); };
